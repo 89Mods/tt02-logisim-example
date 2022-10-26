704428b2de257716901e6c29c7748d63fc03c06d
Tholin <cutie@tholin.dev>
2022-10-27 00:22:47 +0200

    Remove unused files from config (Test 2).

--- src/user_config.tcl
+++ src/user_config.tcl
@@ -1,7 +1,3 @@
 set ::env(DESIGN_NAME) logisim_demo
 set ::env(VERILOG_FILES) "\
-    $::env(DESIGN_DIR)/logisimTopLevelShell.v \
-    $::env(DESIGN_DIR)/circuit/main.v \
-    $::env(DESIGN_DIR)/gates/NAND_GATE.v \
-    $::env(DESIGN_DIR)/gates/OR_GATE.v \
-    $::env(DESIGN_DIR)/memory/D_FLIPFLOP.vv"
+    $::env(DESIGN_DIR)/logisimTopLevelShell.v"
