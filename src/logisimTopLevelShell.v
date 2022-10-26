b219568a75feb4a6ae9364b7873a1580d9dadcce
Tholin <cutie@tholin.dev>
2022-10-27 00:45:06 +0200

    Check if this syntax does actually work.

--- src/logisimTopLevelShell.v
+++ src/logisimTopLevelShell.v
@@ -6,7 +6,7 @@
  **                                                                          **
  *****************************************************************************/
 `default_nettype none
-module logisim_demo (
+module logisim_demo(
   input [7:0] io_in,
   output [7:0] io_out
 );
@@ -14,17 +14,29 @@ module logisim_demo (
    ** The wires are defined here                                                 **
    *******************************************************************************/
    wire s_CLK = io_in[0];
+   wire s_O_0;
+   wire s_O_1;
+   wire s_O_2;
+   wire s_O_3;
+   assign io_out[0] = s_O_0;
+   assign io_out[1] = s_O_1;
+   assign io_out[2] = s_O_2;
+   assign io_out[3] = s_O_3;
    wire s_RST = io_in[1];
-   assign io_out[0] = ~s_CLK;
-   assign io_out[1] = ~s_RST;
-   assign io_out[2] = s_CLK & s_RST;
-   assign io_out[3] = s_RST;
-   assign io_out[7:4] = 0;
 
+   /*******************************************************************************
+   ** The toplevel component is connected here                                   **
+   *******************************************************************************/
   /* main   CIRCUIT_0 (.CLK(s_CLK),
                      .O_0(s_O_0),
                      .O_1(s_O_1),
                      .O_2(s_O_2),
                      .O_3(s_O_3),
                      .RST(s_RST));*/
+      assign s_O_0 = ~s_CLK;
+      assign s_O_1 = ~s_RST;
+      assign s_O_2 = s_CLK & s_RST;
+      assign s_O_3 = s_RST;
+
+      assign io_out[7:4] = 0;
 endmodule
